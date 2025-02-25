d598d0e7fec6c258b1795cbe82496f16cbd02256
santimontironi <santiimontironi@gmail.com>
2025-02-25 20:04:39 -0300

    Se agregaron errores

--- tasks/views.py
+++ tasks/views.py
@@ -5,21 +5,32 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+
 def home(request):
-    return render(request,'home.html')
+    return render(request, "home.html")
+
 
 def signup(request):
     if request.method == "GET":
-        return render(request,'signup.html',{
-            'form':UserCreationForm()
-        })
+        return render(request, "signup.html", {"form": UserCreationForm()})
     else:
         if request.POST["password1"] == request.POST["password2"]:
             try:
-                user = User.objects.create_user(username=request.POST["username"], password=request.POST["password1"])
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
                 user.save()
                 return HttpResponse("User created successfully")
             except:
-                return HttpResponse("Username already exist")
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm(), "errorUser": "User already exists"},
+                )
         else:
-            return HttpResponse("Passwords do not match")
+            return render(
+                request,
+                "signup.html",
+                {"form": UserCreationForm(), "errorPasswords": "Passwords not match"},
+            )
