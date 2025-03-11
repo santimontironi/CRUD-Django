081eb95554b4ff607429d55ab6636198a249f5e3
santimontironi <santiimontironi@gmail.com>
2025-03-11 20:23:08 -0300

    nueva modifiacion en settings.py

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'djangocrud.wsgi.application'
 
 DATABASES = {
     'default': dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
+        default='postgresql://postgres:postgres@localhost:5432/tasks',
         conn_max_age=600
     )
 }
